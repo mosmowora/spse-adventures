1bef8f05e836994818f748f9a1972e851f030ef6
mosmowora <72347633+mosmowora@users.noreply.github.com>
2022-05-08 10:59:20 +0200

    screen update

--- sprites.py
+++ sprites.py
@@ -323,7 +323,6 @@ class Npc(pygame.sprite.Sprite):
         self.animation_loop = 1
         self.movement_loop = 0
         self.max_travel = r.randint(7, 30)
-        self.kacurovanie = False
 
         colors = [0, 99, 198, 297, 396, 495, 594, 693, 792]
         if self.type == "C": self.color = colors[-1]
@@ -457,11 +456,8 @@ class Npc(pygame.sprite.Sprite):
             if hits: self.game.shoes_on()
         elif self.type == "K":
             hits = pygame.sprite.spritecollide(self, self.game.player_sprite, False)
-            if hits and self.game.locker_stuff['boots'] and not self.kacurovanie: 
-                pygame.mixer.Sound.play(self.game.kacurovanie, -1)
-                self.kacurovanie = True
-                print("greee", self.kacurovanie)
-                pygame.time.set_timer(pygame.USEREVENT, )
+            if hits and self.game.locker_stuff['boots']: 
+                pygame.mixer.Sound.play(self.game.kacurovanie)
 
         """else:
     
